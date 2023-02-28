2961cc3a0af262ee50dcc6e0cc7cf0c56729d511
KEI YIANG LIM <angelic.noted_0g@icloud.com>
2023-02-28 15:13:33 +0000

    Added get user / update user and dlete user

--- src/main/java/design/boilerplate/springboot/controller/ApiController.java
+++ src/main/java/design/boilerplate/springboot/controller/ApiController.java
@@ -1,11 +1,9 @@
 package design.boilerplate.springboot.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import design.boilerplate.springboot.model.User;
 import design.boilerplate.springboot.repository.UserRepository;
@@ -24,15 +22,75 @@ public class ApiController {
 	}
 
 	@PostMapping("/api/user/create")
-	public User createCustomUser(@RequestBody User customUser) {
-		return userRepo.save(customUser);
+	public ResponseEntity<?> createCustomUser(@RequestBody User customUser) {
+		String email = customUser.getEmail();
+		String username = customUser.getUsername();
+
+		// check email empty or not / usernamne empty or not
+		if( email.isEmpty() || username.isEmpty()){
+			return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Email / Username is empty");
+		}
+
+		// validation check on existing account exist
+		if(userRepo.existsByEmail(email) || userRepo.existsByUsername(username) ){
+			return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("User with Username / Email already exist");
+		}
+		return ResponseEntity.ok().body(userRepo.save(customUser));
 	}
 
 	// Get the List of User
 	@GetMapping("/api/user/list")
-	public List<User> getAllCustomUser() {
-		return userRepo.findAll();
+	public ResponseEntity<List<User>> getAllCustomUser() {
+		return ResponseEntity.ok().body(userRepo.findAll());
+	}
+
+	// Get Specific user
+	@GetMapping("/api/user/{username}")
+	public ResponseEntity<?> getCustomUser(@PathVariable(value="username") String username) {
+
+		if(!userRepo.existsByUsername(username) ){
+			return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("User does not exist");
+		}
+		return ResponseEntity.ok().body(userRepo.findByUsername(username));
 	}
 
 
+	// Put Specific user
+	@PutMapping("/api/user/{username}")
+	public ResponseEntity<?> updateCustomUser(@RequestBody User customUser) {
+
+		if(!userRepo.existsByUsername(customUser.getUsername())){
+			return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("User does not exist");
+		}
+
+		userRepo.findById(customUser.getId()).map(user -> {
+			user.setUserRole(customUser.getUserRole());
+			user.setName(customUser.getName());
+			user.setEmail(customUser.getEmail());
+			user.setUsername(customUser.getUsername());
+			user.setPassword(customUser.getPassword());
+			return ResponseEntity.ok().body(userRepo.save(user));
+		}).orElseThrow(RuntimeException::new);
+
+		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Error");
+	}
+
+
+	// Put Specific user
+	@DeleteMapping("/api/user/{username}")
+	public ResponseEntity<String> updateCustomUser(@PathVariable(value="username") String username) {
+		// Check if user exist if it does not return error
+		if(!userRepo.existsByUsername(username) ){
+			return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("User does not exist");
+		}
+
+		User user = userRepo.findByUsername(username);
+
+		try {
+			userRepo.deleteById(user.getId());
+			return ResponseEntity.ok().body("Successfully deleted account");
+		}catch (Exception e){
+			return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Error deleting account");
+		}
+	}
 }
