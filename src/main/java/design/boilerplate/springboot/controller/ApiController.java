15efa11de4c2213f420a82e8a1ee88a11d46f66e
KEI YIANG LIM <angelic.noted_0g@icloud.com>
2023-02-28 14:04:40 +0000

    Added create user and list user

--- src/main/java/design/boilerplate/springboot/controller/ApiController.java
+++ src/main/java/design/boilerplate/springboot/controller/ApiController.java
@@ -1,27 +1,38 @@
 package design.boilerplate.springboot.controller;
 
-import org.springframework.http.HttpStatus;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import antlr.collections.List;
 import design.boilerplate.springboot.model.User;
 import design.boilerplate.springboot.repository.UserRepository;
 import io.swagger.v3.oas.annotations.parameters.RequestBody;
 
+import java.util.List;
+
 
 @RestController
 public class ApiController {
+	@Autowired
 	UserRepository userRepo;
 	@GetMapping("/api/sayHello")
 	public ResponseEntity<String> sayHello() {
 		return ResponseEntity.ok("Hello Spring Boot Boilerplate");
 	}
-	
+
+	@PostMapping("/api/user/create")
+	public User createCustomUser(@RequestBody User customUser) {
+		return userRepo.save(customUser);
+	}
+
+	// Get the List of User
+	@GetMapping("/api/user/list")
+	public List<User> getAllCustomUser() {
+		return userRepo.findAll();
+	}
 
 
 }
